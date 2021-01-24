4b789ecf2ffd490c872f4f82044112e65167ef35
ahmad afwan faizi <ahmadafwanfaizi@gmail.com>
2021-01-24 10:11:56 +0700

    tampilan produk

---
+++ app/Controllers/Produk.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Produk extends BaseController
+{
+    public function data()
+    {
+        return view('produk/data');
+    }
+
+    //--------------------------------------------------------------------
+
+}
